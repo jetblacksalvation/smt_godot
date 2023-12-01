fb246f70c7fa20151b7f1ae2b4d209e7af948444
Xx_JetBlackSalvation_xX <91695842+jetblacksalvation@users.noreply.github.com>
2023-12-01 15:11:59 -0700

    Add files via upload

--- GlobalMap.gd
+++ GlobalMap.gd
@@ -1,17 +1,42 @@
 extends Node
 
+
 class GridHandler: 
+	func apply_texture(mesh_instance_node, texture_path):
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(texture_path)
+		texture.create_from_image(image)
+		if mesh_instance_node.material_override:
+			mesh_instance_node.material_override.albedo_texture = texture  
+	func no_op():
+		pass
+	func brickDrawer():
+		var m = MeshInstance.new()
+		m.mesh = CubeMesh.new();
+		apply_texture(m, 'Icon.png')
+		return m;
+		
+	#private functions above...
 	var map_list = [
 		[1,1,1,1,1],
 		[1,0,0,0,0],
 		[1,0,0,0,0],
 		[1,1,1,1,1]
 	]
+	var map_handler ={
+		0 : funcref(self,'no_op'),
+		1 : funcref(self, "brickDrawer")
+			
+	}
 	var player_pos = [2,2]
 	func __init__():
 		pass
 	func getMapList():
 		return map_list
+	func getMapHandler():
+		return map_handler
 	func getPlayerPos():
 		return player_pos
+	
 	
--- MeshInstance.gd
+++ MeshInstance.gd
@@ -7,12 +7,12 @@ var object_list = []
 func _ready():
 	for x in MapHandler.getMapList().size():
 		for y in MapHandler.getMapList()[x].size():
-			print(x, ", ",y)
-			var mInstance = MeshInstance.new()
-			var b = CubeMesh.new()
-			mInstance.mesh=b;
-			object_list.append(mInstance)
-			mInstance.transform.origin = Vector3(1 * (x-MapHandler.getPlayerPos()[0]), 0, 1* (y-MapHandler.getPlayerPos()[1]))
+			var handler = MapHandler.getMapHandler()
+			var handler_key = MapHandler.getMapList()[x][y]
+			var instance = handler[handler_key].call_func()
+			if instance != null:
+				object_list.append(instance)
+				instance.transform.origin = Vector3((x-MapHandler.getPlayerPos()[0])*2 , 0, (y-MapHandler.getPlayerPos()[1])*2)
 	for x in object_list:
 		print("added ",x)
 		add_child(x)
